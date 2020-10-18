7374852f48ec9bfb4f10c6b8b17e3fc21138c70c
Nathaniel Johnson <joh13579@umn.edu>
2020-10-18 15:37:36 -0500

    Add margin and max width

--- src/components/ResourcePreview.tsx
+++ src/components/ResourcePreview.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import {Card, CardContent, CardHeader} from "@material-ui/core";
 import {useHistory} from "react-router";
 
@@ -16,13 +17,20 @@ const ResourcePreview: React.FC<PreviewProps> = (props) => {
   }
 
   return (
+    <Container>
       <Card onClick={navigate}>
-        <CardHeader title={props.title}/>
+        <CardHeader title={props.title} />
         <CardContent>
           {props.children}
         </CardContent>
       </Card>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  margin: 1em;
+  max-width: 400px;
+`;
 
 export default ResourcePreview;
