019c378711ed7118e5bd9136c0d4c9a0ee21a9ba
Nathaniel Johnson <joh13579@umn.edu>
2020-10-18 15:37:52 -0500

    Display pokemon preview

--- src/pages/Preview.tsx
+++ src/pages/Preview.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import {Grid} from "@material-ui/core";
-import BerryPreview from "../components/BerryPreview";
+import BerryPreview from "../components/BerryPreview"
 import ResourcePreview from "../components/ResourcePreview";
+import PokemonPreview from "../components/PokemonPreview"
 
 const Preview: React.FC = () => {
   return (
-      <Grid container alignContent="center" direction="column" lg={"auto"}>
+      <Grid container justify="center" alignContent="center" direction="row" lg={"auto"}>
         <ResourcePreview title={"Berries"} route={"/berry"}>
           <BerryPreview />
+        </ResourcePreview>
+        <ResourcePreview title={"Pokemon"} route={"/pokemon"}>
+          <PokemonPreview />
         </ResourcePreview>
       </Grid>
   )
